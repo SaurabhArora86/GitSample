21350d18f2caa0000cfcd0b4df855fb27a0ed4c4
GUR38490 <GUR38490@GURM38490.AD.ARICENT.COM>
2017-12-22 15:57:36 +0530

    test

--- SampleMavenPrGit/src/test/java/secondpackage/NewTest.java
+++ SampleMavenPrGit/src/test/java/secondpackage/NewTest.java
@@ -1,6 +1,5 @@
 package secondpackage;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -14,25 +13,27 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  WebDriver driver;
-  String url = null;
-  String userid = null;
-  String password = null;
-  String email = null;
+	WebDriver driver;
+	String url = null;
+	String userid = null;
+	String password = null;
+	String email = null;
+
 	@Test
-  public void secondmethod() throws InterruptedException, IOException {
-	InputStream fis = NewTest.class.getClassLoader().getResourceAsStream("config.properties");;
+	public void secondmethod() throws InterruptedException, IOException {
+	InputStream fis = NewTest.class.getClassLoader().getResourceAsStream("config.properties");
+	;
 	Properties prop = new Properties();
 	prop.load(fis);
 	url = prop.getProperty("url");
 	userid = System.getenv("userid");
 	password = System.getenv("password");
-	System.out.println("Password : "+password);
+	System.out.println("Password : " + password);
 	email = prop.getProperty("email");
-  System.setProperty("webdriver.chrome.driver","D:\\Projects\\Selenium Work\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "D:\\Projects\\Selenium Work\\chromedriver.exe");
 	driver = new ChromeDriver();
 	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	String baseURL= url;
+	String baseURL = url;
 	driver.navigate().to(baseURL);
 	WebElement element1 = driver.findElement(By.name("email"));
 	WebElement element2 = driver.findElement(By.name("pass"));
@@ -41,11 +42,12 @@ public class NewTest {
 	element2.sendKeys(password);
 	driver.findElement(By.xpath("//*[@id='loginbutton']")).click();
 	System.out.println("secondmethod function");
-  }
+	}
+
 	@AfterTest
-	void finalfunction2(){
+	void finalfunction2() {
 	driver.quit();
 	System.out.println("Under final After test");
 
-  }
+	}
 }
